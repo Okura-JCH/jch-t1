b8dd932552a3c00f138cdf0ac98b76d758d0f799
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2024-03-14 14:48:37 +0000

    Refactor page component to include Swiper and update layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+'use client'
 import Image from "next/image";
-import Link from "next/link"; // 追加
+import Link from "next/link";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 export default function Home() {
-  // 写真、ヘッドライン、説明のセットを含む配列
   const items = [
     {
       imageUrl: "https://placehold.jp/300x300.png",
@@ -38,7 +43,45 @@ export default function Home() {
   ];
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 m-8">
+    <div className="mt-4 mb-4 ml-8 mr-8  min-h-screen">
+      <h2 className="text-2xl font-bold mb-4">人気のやつ</h2>
+      <Swiper
+        spaceBetween={100}
+        centeredSlides
+        slidesPerView={4}
+        speed={1000}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation
+        loop
+        modules={[Autoplay, Pagination, Navigation]}
+      >
+        {items.map((item, index) => (
+          <SwiperSlide key={index}>
+            <Link 
+              href={{
+                pathname: "/details",
+                query: item,
+              }}
+              passHref
+            >
+              <div className="flex flex-col items-center space-y-4 cursor-pointer mb-8">
+                <Image src={item.imageUrl} width={300} height={300} alt="画像の説明" />
+                <div className="space-y-2 w-full max-w-xs">
+                  <h3 className="text-xl font-bold">{item.headline}</h3>
+                </div>
+              </div>
+            </Link>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <h2 className="text-2xl font-bold mb-4">新着とか</h2>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 ld:grid-cols-5 gap-4 m-8">
       {items.map((item, index) => (
         <Link 
           key={index} 
@@ -52,11 +95,11 @@ export default function Home() {
             <Image src={item.imageUrl} width={300} height={300} alt="画像の説明" />
             <div className="space-y-2 w-full max-w-72">
               <h2 className="text-xl font-bold">{item.headline}</h2>
-              <p>{item.description}</p>
             </div>
           </div>
         </Link>
       ))}
+    </div>
     </div>
   );
 }
